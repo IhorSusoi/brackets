f5291d30090f156eb90f5476a02c22bfca6cb313
Ihor Susoi <ihor.susoi@gmail.com>
2023-02-10 22:08:55 +0200

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    //додавати в масив відкриті дужки, якшо попадеться закрита то перевірити чи вона така сама як остання з відкритих,
+    // і видалити її з масиву
+    let arr=[];
+    let elemNumb=0;
+    for(let i=0; i<str.length; i++){
+        for(let j=0; j<bracketsConfig.length; j++){
+            if(str[i]==bracketsConfig[j][1]){
+                if(arr[elemNumb-1]==bracketsConfig[j][0]){
+                    arr.pop();
+                    elemNumb--;
+                } else {
+                    arr.push(str[i]);
+                    elemNumb++
+                }
+            } else {
+                if(str[i]==bracketsConfig[j][0]){
+                    arr.push(str[i]);
+                    elemNumb++
+                }
+            }
+        }
+    }
+    return arr.length==0;
 }
